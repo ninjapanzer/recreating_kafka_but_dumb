0684d6739fe15afea5d517259bcc9d6e41feed41
Paul Scarrone <paul@scarrone.co>
2024-11-24 21:59:41 -0500

    test

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
-.PHONY: build-e2e build-server build test
+.PHONY: build-ui build-e2e build-server build test
 test:
 	go test -v ./...
+build-ui:
+	go build -o gui ./cmd/ui/ui.go
 build-e2e:
 	go build -o e2e ./cmd/e2e/e2e.go
 build-server:
 	go build -o server ./cmd/server.go
-build: test build-e2e build-server
+build: test build-ui build-e2e build-server
--- flake.nix
+++ flake.nix
@@ -1,27 +1,29 @@
 {
   description = "Go Event Streaming env";
 
-  inputs.nixpkgs = {
-    type = "github";
-    owner = "ninjapanzer";
-    repo = "nixpkgs";
-    rev = "ee8a7accf156e3c1bde14f21f6acfc14ccec133f";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    giopkg.url = "sourcehut://git.sr.ht/~eliasnaur/gio";
   };
 
-  # inputs.nixpkgs.url = "/home/paulscoder/repos/nixpkgs";
-
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, giopkg }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
+        giopkgDevShell = giopkg.outputs.devShells.${system}.default;
       });
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
+      devShells = forEachSupportedSystem ({ pkgs, giopkgDevShell }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
+            gnumake
           ];
+
+#          inputsFrom = [
+#            giopkgDevShell
+#          ];
         };
       });
     };
